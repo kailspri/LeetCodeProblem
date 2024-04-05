ac2f15dabc80159be3c7d0814dcab9c87be4ee88
kailspri <kailsprihan@gmail.com>
2024-04-05 22:34:57 +0530

    Longest Common Subsequence

---
+++ LongestCommonSubsequence/src/leetcode/com/LCS.java
@@ -0,0 +1,24 @@
+package leetcode.com;
+
+public class LCS {
+
+	public static void main(String[] args) {
+		String [] strs={"flower","flow","flowed"};
+		String LCS=longestCommonPrefix( strs);
+		System.out.println(LCS);
+
+	}
+
+	private static String longestCommonPrefix(String[] strs) {
+		String prefix=strs[0];
+		for(int i=1;i<strs.length;i++) {
+			System.out.println("Prefix bfore while "+i +prefix);
+			while(strs[i].indexOf(prefix)!=0) {
+				prefix=prefix.substring(0, prefix.length()-1);
+				System.out.println("Prefix in while "+i +prefix);
+			}
+		}
+		return prefix;
+	}
+
+}
