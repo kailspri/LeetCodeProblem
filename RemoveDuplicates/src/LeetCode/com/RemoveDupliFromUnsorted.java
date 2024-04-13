8260d0db60f016a7cbbbf666f94c5d5e61ab276f
kailspri <kailsprihan@gmail.com>
2024-04-13 15:43:22 +0530

    removing duplicated from an unsorted array LeetCode Method tried

--- RemoveDuplicates/src/LeetCode/com/RemoveDupliFromUnsorted.java
+++ RemoveDuplicates/src/LeetCode/com/RemoveDupliFromUnsorted.java
@@ -8,14 +8,41 @@ public class RemoveDupliFromUnsorted {
 
 	public static void main(String[] args) {
 		
-		int [] arr = {1,1,1};
-		int arrLength=removeDuplicate(arr);
+		int [] arr = {1,2,1};
+		//int arrLength=removeDuplicate(arr);
+		//Other way using Arrays.Sort method 
+		int arrLength=removeDuplicate1(arr);
 		
 		System.out.println(arrLength);
 		for (int i = 0; i < arrLength; i++) {
              System.out.print(arr[i] + " ");
 		}
        }
+
+	private static int removeDuplicate1(int[] arr) {
+		Arrays.sort(arr);
+		int uniqueIndex=0;
+		for(int i =1;i<arr.length;i++) {
+			if(arr[i]!=arr[uniqueIndex]) {
+				uniqueIndex++;
+				arr[uniqueIndex]=arr[i];
+			}
+		}
+		return uniqueIndex+1;
+		
+	}
+	private static int removeDuplicateLeetCode(int[] arr) {
+		Arrays.sort(arr);
+		int uniqueIndex=1;
+		for(int i =0;i<arr.length-1;i++) {
+			if(arr[i]!=arr[i+1]) {
+				uniqueIndex++;
+				arr[uniqueIndex]=arr[i+1];
+			}
+		}
+		return uniqueIndex;
+		
+	}
 
 	private static int removeDuplicate(int[] arr) {
 		int uniqueIndex=0;
@@ -28,7 +55,6 @@ public class RemoveDupliFromUnsorted {
 		}
 		System.out.println(set);
 		return uniqueIndex ;
-	
 	}
 
 }
