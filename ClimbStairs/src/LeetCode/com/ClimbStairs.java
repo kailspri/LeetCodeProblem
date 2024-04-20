dd0cff4afd04251cd5793624f061cb890d232292
kailspri <kailsprihan@gmail.com>
2024-04-20 13:31:54 +0530

    climb stairs

---
+++ ClimbStairs/src/LeetCode/com/ClimbStairs.java
@@ -0,0 +1,29 @@
+package LeetCode.com;
+
+public class ClimbStairs {
+
+	public static void main(String[] args) {
+	 int stairs=4;
+	 int noOfWays=climbStairs( stairs);
+	 System.out.println(noOfWays);
+	 
+
+	}
+
+	private static int climbStairs(int stairs) {
+		int one =1,two=1;
+		int temp;
+		for(int i =0;i< stairs;i++) {
+//			temp= one ;
+//			one=one + two;
+//			two=temp;
+			temp=one+two;
+			one=two;
+			two=temp;
+			
+		}
+		
+		return one;
+	}
+
+}
--- Sqrtx/src/LeetCode/com/SqrtX.java
+++ Sqrtx/src/LeetCode/com/SqrtX.java
@@ -3,7 +3,7 @@ package LeetCode.com;
 public class SqrtX {
 
 	public static void main(String[] args) {
-		int x =8;
+		int x =16;
 		int sqrt=mySqrt( x);
 		System.out.println(sqrt);
 	}
@@ -13,16 +13,19 @@ public class SqrtX {
 		int end=x;
 		while(start<=end) {
 			int mid=start+(end-start)/2;
-			System.out.println("Start : "+start+"END : "+end +"MID :"+mid);
+		System.out.println("Start : "+start+"END : "+end +"MID :"+mid);
 			if(mid==x/mid)
 				 return mid;
 			else if(mid<x/mid)
 				 start=mid+1;
 			else
 				end=mid-1;
-			
 		}
+//		return start;
 		return end;
+//		Finally, when the loop exits, the algorithm returns end. This is because end holds the largest integer value less than or equal
+//		to the square root of x within the search range.
+//		So, end is the closest integer approximation of the square root of x.
 	}
 
 }
