bde53c53ca6a3c28c11aa465aa7a4c42b48c44d3
kailspri <kailsprihan@gmail.com>
2024-04-20 14:28:15 +0530

    Add Binary Done Using IntToBinary built In method and Math.pow

--- AddBinary/src/LeetCode/com/AddBinary.java
+++ AddBinary/src/LeetCode/com/AddBinary.java
@@ -1,16 +1,55 @@
 package LeetCode.com;
 
+import java.util.Arrays;
+
 public class AddBinary {
 
 	public static void main(String[] args) {
-		String a ="11";
-		String b="1";
+		String a ="1010";
+		String b="1011";
 		String output=addBinary(a, b);
-		System.out.println();
+		
+		System.out.println(output);
 	}
 
 	private static String addBinary(String a, String b) {
-		
-		return null;
+		int a1=convertBinaryToDouble(a);
+		int b1=convertBinaryToDouble(b);
+		int sum=a1+b1;
+		String ans=convertDoubletoBinary(sum);
+		System.out.println("sum"+sum );
+		return ans;
+	}
+
+	private static String convertDoubletoBinary(int sum) {
+        int num= sum;
+//        String ans=Integer.toBinaryString(a);
+        int []storeBinary=new int[32];
+        int index=0;
+        while(num>0) {
+        	storeBinary[index++]=num%2;
+        	num=num/2;
+        }
+        System.out.print("Binary representation of the integer is: ");
+     // Build binary string
+        StringBuilder binaryStringBuilder = new StringBuilder();
+        for (int i = index - 1; i >= 0; i--) {
+            binaryStringBuilder.append(storeBinary[i]);
+        }
+        
+        // Return binary string
+        return binaryStringBuilder.toString();
+	}
+
+	private static int convertBinaryToDouble(String a) {
+		double sum=0;int pow=0;
+		for(int i =a.length()-1;i>=0;i--) {
+			if(a.charAt(i)=='1') {
+				sum=sum+Math.pow(2, pow);				
+			}
+			pow++;
+		}
+		System.out.println(sum);
+		return (int) sum;
 	}
 }
