3d916adbced00814da44226d10b4d0bf3cfcefc2
kailspri <kailsprihan@gmail.com>
2024-05-26 15:31:36 +0530

    prob 135 candy using LtR and RtL scanning o(n) space complexity

---
+++ Candy/src/com/LeetCode/Candy.java
@@ -0,0 +1,45 @@
+package com.LeetCode;
+
+import java.util.Arrays;
+
+/*Input: ratings = [1,0,2]
+Output: 5
+Explanation: You can allocate to the first,
+second and thirdchild with 2, 1, 2 candies respectively.*/
+public class Candy {
+	public static void main(String[] args) {
+		int[] ratings= {1,0,2};
+		int candies= candy(ratings);
+		System.out.println(candies);
+	}
+
+	//space and time comlexity using o(n)
+	private static int candy(int[] ratings) {
+		int n =ratings.length;
+		int [] candies= new int[ n];
+		
+	// Initialize candies array with 1 for each child
+		Arrays.fill(candies, 1);
+		
+	// Scan from left to right, update candies according to ratings
+		for(int i=1;i<n;i++) {
+			if(ratings[i]>ratings[i-1]) {
+				candies[i]=candies[i-1]+1;
+			}
+		}
+		
+		for(int i=n-2;i>=0;i--) {
+			if(ratings[i]>ratings[i+1]) {
+				candies[i]=Math.max(candies[i], candies[i+1]+1);
+			}
+		}
+		
+		 // Sum up all candies
+		int sum=0;
+		for(int candy :candies) {
+			sum+=candy;
+		}
+		return sum;
+	}
+
+}
